4db7c437f3b349ead1db72a9e5fe25db3c228917
Peter Martin <peterjmartin2000@gmail.com>
2018-01-24 12:01:16 -0600

    I did some stuff.  Messed with Angular, but styles might be helpful.
    This is something I did.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ProfileComponent } from './profile/profile.component';
+import { HeadingComponent } from './heading/heading.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ProfileComponent,
+    HeadingComponent
   ],
   imports: [
     BrowserModule
