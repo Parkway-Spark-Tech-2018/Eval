200f9d3117d8a89834f84165f809c2c9b19ae9b9
Mechasparrow <mikipux7@gmail.com>
2018-01-25 21:43:53 -0600

    added ability to view specific teacher *kinda*

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 import {Router, ActivatedRoute, ParamMap, NavigationExtras} from '@angular/router';
 
+import {Review} from '../../models/Review';
+
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
@@ -11,13 +13,38 @@ export class ProfileComponent implements OnInit {
 
   public teacher_name:string;
 
+  public reviews:Review[] = [];
+
   constructor(
     private route: ActivatedRoute,
     private router: Router
   ) { }
 
   ngOnInit() {
-    
+
+    let that = this;
+
+    this.getTeacherName().then (function (teacher_name) {
+      that.teacher_name = teacher_name;
+    })
+
+  }
+
+  getTeacherName() {
+
+    var that = this;
+
+    let query_promise = new Promise (function (resolve, reject) {
+      that.route
+      .queryParams
+      .subscribe(params => {
+        let teacher_name = params["teacher_name"] || null;
+        resolve(teacher_name);
+      })
+    });
+
+    return query_promise;
+
   }
 
   leave_review() {
