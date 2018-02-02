eaa69658ed0a7a275882bf849c67e7b82d4a8566
Michael Navazhylau <mikipux7@gmail.com>
2018-02-02 12:58:02 -0600

    working on seeding

--- db_seed/seed.php
+++ db_seed/seed.php
@@ -1,5 +1,25 @@
 <?php
 
+  $pdo = new PDO('mysql:localhost', 'root', 'test');
+  $seeder = new \tebazil\dbseeder\Seeder($pdo);
+
+  $teachers_json = file_get_contents("teachers.json");
+  $teachers = json_decode($teachers_json, true)["Sheet1"];
+
+  $courses_json = file_get_contents("courses.json");
+  $courses = json_decode($courses_json, true);
+
+  $departments_json = file_get_contents("departments.json");
+  $departments = json_decode($departments_json, true)["Sheet1"];
+
+  //print_r ($teachers);
+
+  //print_r($courses);
+
+  //print_r($departments);
+
+
+
 
 
  ?>
