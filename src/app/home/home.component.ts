e8c79cabe12eab54b0f4637ec7a58d63cc24de3b
Peter Martin <peterjmartin2000@gmail.com>
2018-02-06 12:39:34 -0600

    User-Specific Page Test

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from "../providers/auth.service";
+import { Router} from "@angular/router";
+import {AngularFireAuth} from 'angularfire2/auth';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [AuthService, AngularFireAuth]
 })
 export class HomeComponent implements OnInit {
 
@@ -12,13 +17,26 @@ export class HomeComponent implements OnInit {
   public teachers = []
 
   public search_string:string = "";
+  public router: Router;
 
-  constructor() { }
+  constructor(private auth:AuthService) { }
 
   ngOnInit() {
+    console.log('At least I got here though');
+    console.log(this.auth.getUser().user_name);
+    if (this.auth.getUser().type == 1) //Student
+    {
+      console.log('Well you are a sudent, so, going to search page.')
+      this.router.navigate(['/search']);
+    }
+    if (this.auth.getUser().type == 2) //Teacher
+    {
+      console.log('Well you are a sudent, so, going to search page.')
+      this.router.navigate(['/about']);
+    }
 
   }
 
-  
+
 
 }
