600147f0fa98734ec389c23cf28140c0df98c393
Mechasparrow <mikipux7@gmail.com>
2018-02-13 20:49:19 -0600

    added session controller

---
+++ src/Controller/SessionController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class SessionController extends Controller
+{
+
+    public function index()
+    {
+        return new Response('Welcome to your new controller!');
+    }
+
+    public function show()
+    {
+
+      $conn = $this->getDoctrine()->getEntityManager()->getConnection();
+
+      $sql = 'SELECT * FROM Session';
+
+      $stmt = $conn->prepare($sql);
+      $stmt->execute();
+
+      $results = $stmt->fetchAll();
+
+      return new JsonResponse($results);
+
+    }
+
+}
