bef7825a8c63165e13f87eac7b3c7c171a6c76e8
Mechasparrow <mikipux7@gmail.com>
2018-02-13 21:08:19 -0600

    fix

--- src/Kernel.php
+++ src/Kernel.php
@@ -7,6 +7,7 @@ use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 use Symfony\Component\Routing\RouteCollectionBuilder;
+use Symfony\Bundle\WebServerBundle;
 
 class Kernel extends BaseKernel
 {
