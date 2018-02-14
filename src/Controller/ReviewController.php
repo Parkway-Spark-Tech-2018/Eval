85bbb062084c3f49e3e3b410053abb9308dfba84
Mechasparrow <mikipux7@gmail.com>
2018-02-13 22:41:31 -0600

    lot o code

--- src/Controller/ReviewController.php
+++ src/Controller/ReviewController.php
@@ -6,6 +6,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 class ReviewController extends Controller
 {
@@ -27,6 +28,35 @@ class ReviewController extends Controller
       $results = $stmt->fetchAll();
 
       return new JsonResponse($results);
+
+    }
+
+    public function new() {
+
+      $request = Request::createFromGlobals();
+
+      $method = $request->getMethod();
+
+      $arr = array (
+        "status" => "good"
+      );
+
+      $err = array (
+        "status" => "404"
+      );
+
+      if ($method == "POST") {
+        $form_data = json_decode(file_get_contents('php://input'), true);
+
+        ## TODO Implement Review Creation here pls
+
+
+        ##
+
+        return new JsonResponse($form_data);
+      }else {
+        return new JsonResponse($err);
+      }
 
     }
 
