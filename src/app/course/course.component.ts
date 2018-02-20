dc2813eab905ffe3a0544e4db77562dffebfc97f
Michael Navazhylau <mikipux7@gmail.com>
2018-02-20 13:02:08 -0600

    meh

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -57,7 +57,7 @@ export class CourseComponent implements OnInit {
     }).then (function (teachers: Teacher[]) {
       console.log(teachers);
       that.teachers = teachers;
-    }
+    });
 
   }
 
