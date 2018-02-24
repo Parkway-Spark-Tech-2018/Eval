7b2c43098ac1162d124e14b468c2cfde6b360384
Michael Navazhylau <mikipux7@gmail.com>
2018-02-23 19:37:43 -0600

    added Student Controller

---
+++ src/Controller/StudentController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class StudentController extends Controller
+{
+
+    public function index()
+    {
+        return new Response('Welcome to your new controller!');
+    }
+
+    public function show()
+    {
+      $conn = $this->getDoctrine()->getEntityManager()->getConnection();
+
+      $sql = 'SELECT * FROM Student';
+
+      $stmt = $conn->prepare($sql);
+      $stmt->execute();
+
+      $results = $stmt->fetchAll();
+
+      return new JsonResponse($results);
+    }
+
+
+}
